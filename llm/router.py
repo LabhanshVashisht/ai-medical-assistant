eb49c5b0fe3aac37c9e49bb8a5ed988605ad85da
LabhanshVashisht <labhanshvashisht2007@gmail.com>
2026-01-20 20:03:03 +0530

    Fixed Gemini not working and broken responses

--- llm/router.py
+++ llm/router.py
@@ -1,4 +1,4 @@
 from llm.gemini_llm import gemini_response
 
-def get_llm_response(system, messages_history, image_data=None):
-    return gemini_response(system, messages_history, image_data)
+def get_llm_response(system_prompt, messages, image=None):
+    return gemini_response(system_prompt, messages, image)
